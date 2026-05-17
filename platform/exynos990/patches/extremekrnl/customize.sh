e9c10ffa87de0ac3655216a563d365b2cea43338
Miguel Barreto <miguel03barreto@gmail.com>
2026-05-17 12:43:50 -0300

    Adds ArtisanKRNL repo in customize.sh

--- platform/exynos990/patches/extremekrnl/customize.sh
+++ platform/exynos990/patches/extremekrnl/customize.sh
@@ -1,5 +1,5 @@
 # [
-EXTREMEKRNL_REPO="https://github.com/ExtremeXT/990_upstream_v2/"
+EXTREMEKRNL_REPO="https://github.com/Android-Artisan/android_kernel_samsung_exynos990.git"
 
 BUILD_KERNEL()
 {
